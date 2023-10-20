10ef0ca9fbd3c0a94e90ea0d7f8c617ed0a2612e
francimaraoliveira <61793314+francimaraoliveira@users.noreply.github.com>
2023-10-19 23:24:03 -0300

    Add files via upload

---
+++ Like-Dislike-app/like-dislike/src/components/BotaoVoto.tsx
@@ -0,0 +1,20 @@
+// src/components/BotaoVoto.tsx
+
+import React from 'react';
+import './BotaoVoto.css';
+
+interface BotaoVotoProps {
+  tipo: 'UP' | 'DOWN';
+  onClick: () => void;
+  className?: string; // Adicionando a propriedade className
+}
+
+const BotaoVoto: React.FC<BotaoVotoProps> = ({ tipo, onClick, className }) => {
+  return (
+    <button onClick={onClick} className={`botao-${tipo.toLowerCase()} ${className}`}>
+      {tipo}
+    </button>
+  );
+};
+
+export default BotaoVoto;
---
+++ Like-Dislike-app/like-dislike/src/components/ListaTopicos.tsx
@@ -0,0 +1,35 @@
+// src/components/ListaTopicos.tsx
+
+import React from 'react';
+import { Topico } from '../types';
+import BotaoVoto from './BotaoVoto';
+import './ListaTopicos.css';
+
+interface ListaTopicosProps {
+  topicos: Topico[];
+  onVoto: (topicoId: string, tipo: 'UP' | 'DOWN') => void;
+  calcularVotos: (topico: Topico) => { favoraveis: number, desfavoraveis: number };
+}
+
+const ListaTopicos: React.FC<ListaTopicosProps> = ({ topicos, onVoto, calcularVotos }) => {
+  return (
+    <div className="topicos-container">
+      {topicos.map((topico) => (
+        <div key={topico.id} className={`topico topico-item ${topico.active ? '' : 'inativo'}`}>
+          <h3>{topico.descricao}</h3>
+          <p>Autor: {topico.autor.nome}</p>
+          <p>Tags: {topico.tags.join(', ')}</p>
+          <p>Ativo: {topico.active ? 'Sim' : 'Não'}</p>
+          <p>Total de Votos: {topico.votos.length}</p>
+          <p>Votos Favoráveis: {calcularVotos(topico).favoraveis}</p>
+          <p>Votos Desfavoráveis: {calcularVotos(topico).desfavoraveis}</p>
+          <p>Saldo Atual: {topico.votos.filter((voto) => voto.tipo === 'UP').length - topico.votos.filter((voto) => voto.tipo === 'DOWN').length}</p>
+          <BotaoVoto tipo="UP" onClick={() => onVoto(topico.id, 'UP')} className={topico.active ? 'botao-up' : ''} />
+          <BotaoVoto tipo="DOWN" onClick={() => onVoto(topico.id, 'DOWN')} className={topico.active ? 'botao-down' : ''} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ListaTopicos;
